84263d09634d1289f4f10294620c9842610ef38f
Pegah Eizadkhah <peizadkh@gmail.com>
2018-05-18 20:04:02 -0400

    working demo.

--- src/components/rate-calculator.js
+++ src/components/rate-calculator.js
@@ -6,14 +6,30 @@ import Output from './output';
 export default class RateCalculator extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+          dayRate: 300,
+          hoursWork: 5,
+        };
+    }
+
+    setDayRate(dayRate){
+      this.setState({
+        dayRate
+      });
+    }
+
+    setHoursWork(hoursWork){
+      this.setState({
+        hoursWork
+      });
     }
 
     render() {
-        const rate = 10;
+        const rate = this.state.dayRate/this.state.hoursWork;
         return (
             <form>
-                <NumberInput id="day-rate" label="Day rate" min={0} max={5000} />
-                <NumberInput id="hours" label="Hours" min={1} max={12} />
+                <NumberInput id="day-rate" label="Day rate" value={this.state.dayRate} valueChange={value => this.setDayRate(value)} min={0} max={5000} />
+                <NumberInput id="hours" label="Hours" value={this.state.hoursWork} valueChange={value => this.setHoursWork(value)} min={1} max={12} />
                 <Output id="hourly-rate" label="Hourly rate" value={rate.toFixed(2)}
 />
             </form>
